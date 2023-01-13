f456bd11790e0db013693d8568e7233ad75011fa
Katerina <tkachenko.city@gmail.com>
2023-01-13 22:50:59 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
-import { getFilms } from './getTrendFilms'
+import { getFilms } from './partials-js/getTrendFilms';
 
-document.addEventListener("DOMContentLoaded", getFilms());
+document.addEventListener('DOMContentLoaded', getFilms());
 
 export function makerender(arr) {
-    console.log(arr);
+  console.log(arr);
 }
-
