5a2481c5c6564954b65e388347fa004118051580
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-01-17 11:21:28 +0100

    update maim fetch
    add functions makerender, loadPage, gettrend film

---
+++ src/partials-js/makeRenderFilms.js
@@ -0,0 +1,82 @@
+import { MOBILE_SIZES, DESKTOP_SIZES, TABLET_SIZES, BASE_URL, API_KEY } from './vars';
+
+const testEl = document.querySelector('.test')
+document.addEventListener('DOMContentLoaded', makerender());
+
+export function makerender(arr) {
+
+    console.log(arr);
+
+   const renderEl = arr.map(({ id, original_title, release_date }) => {
+      return `<li class="movie__card">
+        <a class="movie__link open__modal--js link"  data-id="${id}" href="">
+          <div class="movie__img__box">
+           <picture class="film-list__img">
+            <source
+                srcset="
+                ${BASE_URL}${DESKTOP_SIZES}${API_KEY}"
+                media="screen and (min-width:1200px)"
+            />
+            <source
+                srcset="
+                ${BASE_URL}${TABLET_SIZES}${API_KEY} "
+                media="(min-width:768px)"
+            />
+            <source
+                srcset="
+                ${BASE_URL}${MOBILE_SIZES}${API_KEY}"
+                media="(max-width:767px)"
+            />
+            <img
+              class="movie__img"
+              src="./images/no-Film-Img.jpg"
+              alt="${original_title}"
+              width="394"
+              height="574"
+              name="Poster"
+            />
+        </picture>           
+        </div>
+        </a>
+        <div class="movie__card__textbox">
+          <a class="movie__link open__modal--js link" data-id="${id}" href="">
+            <h3 class="movie__title">${original_title}</h3>
+            <span class="movie__details">Жанр| </span
+            ><span class="movie__details">${release_date.slice(0, 4)}</span>
+          </a>
+        </div>
+      </li> `;
+   });
+    
+
+    testEl.insertAdjacentHTML('beforeend', renderEl.join(''))
+  
+}
+
+// console.log(arr);
+  // arr.forEach(
+  //   ({ backdrop_path, poster_path, original_title, release_date }) => {
+  //     const renderEl = `<li class="movie__card">
+  //          <a class="movie__link open__modal--js" data-id="${id}" href="">
+  //               <div class="movie__img__box">
+  //                   <img class="movie__img"
+  //                   src='${BASE_URL}${POSTER_SIZES}${poster_path}'
+  //                   alt='${original_title}'
+  //                   name="Poster"
+  //                    >
+  //               </div>
+  //           </a>
+  //           <div class="movie__card__textbox">
+  //               <a class="movie__link open__modal--js" href="">
+  //                   <h3 class="movie__title">${original_title}</h3>
+  //                   <span class="movie__details">Жанри | </span><span
+  //                   class="movie__details">${release_date.slice(0, 4)}</span>
+  //
+  //          </a>
+  //          </div>
+  //      </li>
+  //   `;
+  //
+  // galleryRef.insertAdjacentHTML('beforeend', renderEl);
+  //  }
+  //);
