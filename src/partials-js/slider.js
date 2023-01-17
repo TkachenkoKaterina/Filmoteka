2eedc6f5bc2d776cee7e5c69baf8bfbbbb1969fe
Katerina <tkachenko.city@gmail.com>
2023-01-18 00:16:35 +0200

    slider...

--- src/partials-js/slider.js
+++ src/partials-js/slider.js
@@ -1,81 +1,10 @@
 import Glide from '@glidejs/glide';
 import axios from 'axios';
-// new Glide('.glide').mount();
-
-// import filmsCardSliderTpl from '../templates/card-films-slider.hbs';
-// import trailer from './trailers.js';
-// import { makerender } from './makeRenderFilms';
-import { requestGet } from './requestGet';
-
-import {
-  MAIN_PART_URL,
-  API_KEY,
-  DESKTOP_SIZES,
-  TABLET_SIZES,
-  MOBILE_SIZES,
-  BASE_IMG_URL,
-  GENRE_REQUEST_PART,
-  TRENDS_REQUEST_PART,
-  PATH_TRENDS,
-  SEARCH_MOVIE,
-  POSTER_SIZES,
-} from './vars';
-
-// const containerRef = document.querySelector('.container__slider');
-
-const sliderUlRef = document.querySelector('.glide__slides');
-// console.log(sliderUlRef);
-
-const BtnRef = document.querySelector('.glide__arrow');
-// console.log(BtnRef);
-
-BtnRef.addEventListener('click', () => {
-  requestGet()
-    .then(data => renderSlider(data))
-    .catch(error => console.log(error));
-});
-
-// async function fetchForSlider() {
-//   const { response } = await requestGet(
-//     MAIN_PART_URL,
-//     TRENDS_REQUEST_PART,
-//     API_KEY
-//   );
-//   console.log(response);
-//   console.log(response.data);
-
-//   return response.data;
-// }
-
-function renderSlider(data) {
-  const markup = data
-    .map(results => {
-      return `
-          <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-        <li class="glide__slide"></li>
-      `;
-    })
-    .join('');
-  sliderUlRef.innerHTML = markup;
-}
+import filmsCardSliderTpl from '../templates/card-film-slider.hbs';
+import errorUrl from '../images/oh-no.jpg';
+const sliderContainer = document.querySelector('.js-slider-container');
+console.log(sliderContainer);
+renderTrendy();
 
 const glide = new Glide('.glide', {
   type: 'slider',
@@ -88,24 +17,20 @@ const glide = new Glide('.glide', {
 
 glide.mount();
 
-// async function sliderRender() {
-//   try {
-//     console.log(BASE_IMG_URL);
-//     const response = await axios.get(BASE_IMG_URL);
-//     const arr = response.data.results;
-//     console.log(arr);
-//     // const rusults = response.json();
-//     // console.log(rusults);
-//     makerender(arr);
-//   } catch (error) {
-//     console.log(error);
-//   }
-//   return sliderUlRef.insertAdjacentHTML('beforeend', sliderUlRef);
-// }
+function renderTrendy() {
+  const url = `https://api.themoviedb.org/3/trending/all/day?api_key=d91911ebb88751cf9e5c4b8fdf4412c9`;
+  return fetch(url)
+    .then(response => response.json())
+    .then(({ results }) => {
+      return results;
+    })
+    .then(renderSliderFilms)
+    .catch(err => {
+      console.log('Ошибочка!');
+      sliderContainer.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
+    });
+}
 
-// function makerender(articles) {
-//   sliderUlRef.innerHTML = filmsCardSliderTpl(articles);
-//   trailer.createTrailerLink(document.querySelectorAll('.btn-youtube-slider'));
-// }
-
-//   sliderUlRef.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
+function renderSliderFilms(articles) {
+  sliderContainer.innerHTML = filmsCardSliderTpl(articles);
+}
