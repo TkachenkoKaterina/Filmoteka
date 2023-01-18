f50da481e24de3304870efbfb0391df50462cd52
Katerina <tkachenko.city@gmail.com>
2023-01-18 19:51:15 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ import './partials-js/onLoadPage';
 import './partials-js/themeSwitch';
 import './partials-js/authorization';
 import './partials-js/search-movie-word';
-import './partials-js/axiosAllGenres';
-import './partials-js/axiosFilm';
 import './partials-js/makeRenderFilms';
 import './partials-js/openModal';
 import './partials-js/addToWatch';
@@ -15,7 +13,7 @@ import './partials-js/tuiPagination';
 import './partials-js/footer-modal';
 import './partials-js/buttonToTop';
 import '../node_modules/basiclightbox/dist/basicLightbox.min.css';
-import './partials-js/closePreloader'
+import './partials-js/closePreloader';
 
 import './partials-js/requestGet';
 
