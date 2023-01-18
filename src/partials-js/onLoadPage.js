6abb8edf49bff9c3ce95d6e2a9942da575b10812
DenFedor <fedorenko.wo@gmail.com>
2023-01-18 19:23:02 +0200

    Pagination main page, search
    Added pagination

--- src/partials-js/onLoadPage.js
+++ src/partials-js/onLoadPage.js
@@ -1,22 +1,30 @@
 import { requestGet } from './requestGet';
-import { MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY, GENRE_REQUEST_PART } from './vars';
+import {
+  MAIN_PART_URL,
+  TRENDS_REQUEST_PART,
+  API_KEY,
+  GENRE_REQUEST_PART,
+} from './vars';
 import { makerender } from './makeRenderFilms';
-import { pagination } from './tuiPagination';
+import { pagination, changePage } from './tuiPagination';
 
-
-let arrOfGenres
-function receiveGenres() {
-    requestGet(MAIN_PART_URL, GENRE_REQUEST_PART, API_KEY).then(res => {
-        arrOfGenres = res.data.genres
-    })
-    }
-receiveGenres()
-
+let arrOfGenres;
+export function receiveGenres() {
+  requestGet(MAIN_PART_URL, GENRE_REQUEST_PART, API_KEY).then(res => {
+    arrOfGenres = res.data.genres;
+  });
+}
+receiveGenres();
+let arrFilms;
 export function onLoadPage() {
-    let arrFilms;
-    requestGet(MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY).then((res) => {
-        pagination(res.data.total_pages, res.data.page);
-        arrFilms = res.data.results;
-        makerender(arrFilms, arrOfGenres);
-    })
+  requestGet(MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY).then(res => {
+    arrFilms = res.data.results;
+    makerender(arrFilms, arrOfGenres);
+    const pagInst = pagination(res.data.total_results, res.data.page);
+    pagInst.on('afterMove', function (eventData) {
+      changePage(res.request.responseURL, eventData.page).then(res => {
+        makerender(res.data.results, arrOfGenres);
+      });
+    });
+  });
 }
