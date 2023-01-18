286b01b24b366ed302594f48460d4e26327b55a2
Oleksii Narovskyi <o.narovskyi@gmail.com>
2023-01-18 20:23:34 +0200

    delete some partials from my-library.js

--- src/my-library.js
+++ src/my-library.js
@@ -3,8 +3,6 @@ import './partials-js/vars';
 import './partials-js/onLoadPage';
 import './partials-js/themeSwitch';
 import './partials-js/authorization';
-import './partials-js/search-movie-word';
-import './partials-js/makeRenderFilms';
 import './partials-js/openModal';
 import './partials-js/addToWatch';
 import './partials-js/watchedQueueSet';
