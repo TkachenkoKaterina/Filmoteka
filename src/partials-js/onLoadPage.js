d09c6fa1f78d9c88e9f7cfe593e2c956519cb1d6
Katerina <tkachenko.city@gmail.com>
2023-01-18 18:06:39 +0200

    slider

--- src/partials-js/onLoadPage.js
+++ src/partials-js/onLoadPage.js
@@ -1,22 +1,26 @@
 import { requestGet } from './requestGet';
-import { MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY, GENRE_REQUEST_PART } from './vars';
+import {
+  MAIN_PART_URL,
+  TRENDS_REQUEST_PART,
+  API_KEY,
+  GENRE_REQUEST_PART,
+} from './vars';
 import { makerender } from './makeRenderFilms';
 import { pagination } from './tuiPagination';
 
-
-let arrOfGenres
+let arrOfGenres;
 function receiveGenres() {
-    requestGet(MAIN_PART_URL, GENRE_REQUEST_PART, API_KEY).then(res => {
-        arrOfGenres = res.data.genres
-    })
-    }
-receiveGenres()
+  requestGet(MAIN_PART_URL, GENRE_REQUEST_PART, API_KEY).then(res => {
+    arrOfGenres = res.data.genres;
+  });
+}
+receiveGenres();
 
 export function onLoadPage() {
-    let arrFilms;
-    requestGet(MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY).then((res) => {
-        pagination(res.data.total_pages, res.data.page);
-        arrFilms = res.data.results;
-        makerender(arrFilms, arrOfGenres);
-    })
+  let arrFilms;
+  requestGet(MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY).then(res => {
+    pagination(res.data.total_pages, res.data.page);
+    arrFilms = res.data.results;
+    makerender(arrFilms, arrOfGenres);
+  });
 }
--- src/partials-js/slider.js
+++ src/partials-js/slider.js
@@ -1,14 +1,13 @@
 import Glide from '@glidejs/glide';
 import axios from 'axios';
 import errorUrl from '../images/oh-no.jpg';
-import { requestGet } from '../partials-js/requestGet';
+import { requestGet } from './requestGet';
 import { MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY } from './vars';
 
-async function slider() {
-  const response = await axios.get(
-    `${MAIN_PART_URL}${TRENDS_REQUEST_PART}${API_KEY}`
-  );
-  console.log(response);
+function sliderFetch() {
+  requestGet(MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY).then(res => {
+    const arr = res.data;
+    console.log(arr);
+  });
 }
-
-slider();
+sliderFetch();
