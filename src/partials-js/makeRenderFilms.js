1a932d6d3d0ed42fb69d813c0648fad6150d0a79
Katerina <tkachenko.city@gmail.com>
2023-01-18 17:25:05 +0200

    slider...

--- src/partials-js/makeRenderFilms.js
+++ src/partials-js/makeRenderFilms.js
@@ -1,25 +1,21 @@
-import {
-    POSTER_SIZES,
-    BASE_IMG_URL,
-} from './vars';
-
+import { POSTER_SIZES, BASE_IMG_URL } from './vars';
 
 const trendsContainerEL = document.querySelector('.movie__collection');
 
 export function makerender(arr, collectioinGenres) {
-  
-  
+  console.log(arr);
   const renderEl = arr
-  .map(({ original_title, release_date, poster_path, genre_ids }) => {
-    
-    let genresInCard = [];
-    genre_ids.map(ID => collectioinGenres.forEach(({ id, name }) => {
-      if (ID === id) {
-          genresInCard.push(name)
-        }
-      }) )
-          
-          return `<li class="movie__card">
+    .map(({ original_title, release_date, poster_path, genre_ids }) => {
+      let genresInCard = [];
+      genre_ids.map(ID =>
+        collectioinGenres.forEach(({ id, name }) => {
+          if (ID === id) {
+            genresInCard.push(name);
+          }
+        })
+      );
+
+      return `<li class="movie__card">
             <a class="movie__link open__modal--js" data-id="" href="">
                 <div class="movie__img__box">
                     <img class="movie__img"
@@ -33,21 +29,25 @@ export function makerender(arr, collectioinGenres) {
             <div class="movie__card__textbox">
                 <a class="movie__link open__modal--js" data-id="" href="">
                     <h3 class="movie__title">${original_title}</h3>
-                    <span class="movie__details">${filterArrGenres(genresInCard)} | </span><span class="movie__details">${release_date.slice(0, 4)}</span>
+                    <span class="movie__details">${filterArrGenres(
+                      genresInCard
+                    )} | </span><span class="movie__details">${release_date.slice(
+        0,
+        4
+      )}</span>
                 
             </a>
             </div>
         </li> `;
-        })
-        .join('');
+    })
+    .join('');
 
-    trendsContainerEL.insertAdjacentHTML('beforeend', renderEl);
+  trendsContainerEL.insertAdjacentHTML('beforeend', renderEl);
 }
 
-
 function filterArrGenres(arrGenres) {
-  if (arrGenres.length > 3){
-    return [arrGenres[0], arrGenres[1], 'others']
-  } else 
-  return arrGenres
+  if (arrGenres.length > 3) {
+    return [arrGenres[0], arrGenres[1], 'others'];
+  }
+  return arrGenres;
 }
--- src/partials-js/slider.js
+++ src/partials-js/slider.js
@@ -1,37 +1,14 @@
-// import Glide from '@glidejs/glide';
-// import axios from 'axios';
-// import filmsCardSliderTpl from '../templates/card-film-slider.hbs';
-// import errorUrl from '../images/oh-no.jpg';
-// const sliderContainer = document.querySelector('.js-slider-container');
-// console.log(sliderContainer);
-// renderTrendy();
+import Glide from '@glidejs/glide';
+import axios from 'axios';
+import errorUrl from '../images/oh-no.jpg';
+import { requestGet } from '../partials-js/requestGet';
+import { MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY } from './vars';
 
-// const glide = new Glide('.glide', {
-//   type: 'slider',
-//   startAt: 0,
-//   perView: 8,
-//   autoplay: 2000,
-//   hoverpause: true,
-//   bound: true,
-// });
+async function slider() {
+  const response = await axios.get(
+    `${MAIN_PART_URL}${TRENDS_REQUEST_PART}${API_KEY}`
+  );
+  console.log(response);
+}
 
-// glide.mount();
-
-// function renderTrendy() {
-//   const url = `https://api.themoviedb.org/3/trending/all/day?api_key=c861fc623ae12b9b041c6dade1c99e57`;
-//   return fetch(url)
-//     .then(response => response.json())
-//     .then(({ results }) => {
-//       console.log(results);
-//       return results;
-//     })
-//     .then(renderSliderFilms)
-//     .catch(err => {
-//       console.log('Ошибочка!');
-//       sliderContainer.innerHTML = `<img class="catch-error-pagination" src="${errorUrl}" />`;
-//     });
-// }
-
-// function renderSliderFilms(cards) {
-//   sliderContainer.innerHTML = filmsCardSliderTpl(cards);
-// }
+slider();
