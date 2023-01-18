4898d557fd5a4c2b215b3a3c825768e046da605e
jullyrud <94559083+jullyrud@users.noreply.github.com>
2023-01-18 11:28:59 +0100

    Update makeRenderFilms.js
    finish genres

--- src/partials-js/makeRenderFilms.js
+++ src/partials-js/makeRenderFilms.js
@@ -6,11 +6,20 @@ import {
 
 const trendsContainerEL = document.querySelector('.movie__collection');
 
-export function makerender(arr, arrOfGenres) {
-    console.log(arrOfGenres);
-    const renderEl = arr
-        .map(({ id, original_title, release_date, poster_path }) => {
-            return `<li class="movie__card">
+export function makerender(arr, collectioinGenres) {
+  
+  
+  const renderEl = arr
+  .map(({ original_title, release_date, poster_path, genre_ids }) => {
+    
+    let genresInCard = [];
+    genre_ids.map(ID => collectioinGenres.forEach(({ id, name }) => {
+      if (ID === id) {
+          genresInCard.push(name)
+        }
+      }) )
+          
+          return `<li class="movie__card">
             <a class="movie__link open__modal--js" data-id="" href="">
                 <div class="movie__img__box">
                     <img class="movie__img"
@@ -24,7 +33,7 @@ export function makerender(arr, arrOfGenres) {
             <div class="movie__card__textbox">
                 <a class="movie__link open__modal--js" data-id="" href="">
                     <h3 class="movie__title">${original_title}</h3>
-                    <span class="movie__details">Жанри | </span><span class="movie__details">${release_date.slice(0, 4)}</span>
+                    <span class="movie__details">${filterArrGenres(genresInCard)} | </span><span class="movie__details">${release_date.slice(0, 4)}</span>
                 
             </a>
             </div>
@@ -33,4 +42,12 @@ export function makerender(arr, arrOfGenres) {
         .join('');
 
     trendsContainerEL.insertAdjacentHTML('beforeend', renderEl);
+}
+
+
+function filterArrGenres(arrGenres) {
+  if (arrGenres.length > 3){
+    return [arrGenres[0], arrGenres[1], 'others']
+  } else 
+  return arrGenres
 }
