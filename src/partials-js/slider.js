864f095715df1d9e5591562cf2257cb9e09f0591
Katerina <tkachenko.city@gmail.com>
2023-01-18 23:32:31 +0200

    slider

--- src/partials-js/slider.js
+++ src/partials-js/slider.js
@@ -2,31 +2,38 @@ import Glide from '@glidejs/glide';
 import axios from 'axios';
 import errorUrl from '../images/oh-no.jpg';
 import { requestGet } from './requestGet';
-import { MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY } from './vars';
+import {
+  MAIN_PART_URL,
+  TRENDS_REQUEST_PART,
+  API_KEY,
+  BASE_IMG_URL,
+  MOBILE_SIZES,
+} from './vars';
 
 const sliderContainer = document.querySelector('.js-slider-container');
 
 sliderFetch();
-// sliderRender();
 
 function sliderFetch() {
   requestGet(MAIN_PART_URL, TRENDS_REQUEST_PART, API_KEY).then(res => {
     const arr = res.data.results;
     console.log(arr);
-    return arr;
+    sliderRender(arr);
   });
 }
 
 function sliderRender(arr) {
   console.log(arr);
   const markup = arr
-    .map(({ backdrop_path }) => {
+    .map(({ poster_path }) => {
       return `
           <li class="glide__slide">
-            <img class="glide__img" src="./images/1.jpg" alt="" />
+            <img class="glide__img" src="${BASE_IMG_URL}${MOBILE_SIZES}${poster_path}" width="300" alt="" />
           </li>
           `;
     })
     .join('');
-  sliderContainer.innerHTML = markup;
+  // sliderContainer.innerHTML = markup;
+  console.log(markup);
+  sliderContainer.insertAdjacentHTML('beforeend', markup);
 }
